1875bb5f6dfe3534f13f935617c683953c5d4c14
franco viterbo <francoviterbo@gmail.com>
2017-05-16 11:46:16 -0500

    now playing route added

--- api/routers/liketorRouter.js
+++ api/routers/liketorRouter.js
@@ -1,7 +1,11 @@
 module.exports = function(app) {
 	const likesController = require('../controllers/likesController');
+	const moviesController = require('../controllers/moviesController');
 
 	app.route('/movies/:movieId/likes')
 		.post(likesController.likeMovie)
 		.get(likesController.getLikesFromMovie);
+
+	app.route('/movies/nowplaying')
+		.get(moviesController.getMoviesNowPlaying);
 };
