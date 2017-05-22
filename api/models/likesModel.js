c5bef69404bafd999f1e56b9d8dd531994df6e06
franco viterbo <francoviterbo@gmail.com>
2017-05-22 15:08:57 -0500

    add and get movieLikes over mongo

--- api/models/likesModel.js
+++ api/models/likesModel.js
@@ -1,4 +1,4 @@
-const db = require('../DBs/redisDb');
+const db = require('../DBs/mongoDb');
 
 exports.add = function(movieId, userId, like,callback) {
 	const data = {
@@ -8,21 +8,19 @@ exports.add = function(movieId, userId, like,callback) {
 		date: new Date().getTime(),
 	};
 
-	db.get().rpush(`likes_movie_${movieId}`, JSON.stringify(data),
-		function(err, result) {
-			console.log(`console00: ${result}`);
-			callback(err, result);
-		});
+	const likesCollection = db.get().collection('movieLikes');
+	likesCollection.insert(data,function(err, result) {
+		console.log('Data Inserted');
+		callback(err, result);
+	});
 };
 
 exports.getAll = function(movieId, callback) {
-	db.get().lrange(`likes_movie_${movieId}`, 0, -1,
-		function (err, result) {
-			console.log('console01: '+ result);
-			callback(err, result.map(function(item) {
-				const data = JSON.parse(item);
-				data.date = new Date(data.date);
-				return data;
-			}));
-		});
+	const likesCollection = db.get().collection('movieLikes');
+	likesCollection.find({'movie':movieId}).toArray(function(err, docs) {
+		callback(err, docs.map(function(item) {
+			item.date = new Date(item.date);
+			return item;
+		}));
+	});
 };
